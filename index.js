abfcace0f5a54fe458d6f94cd0dbb61600acfb4a
herjara66 <jaramillojesush@gmail.com>
2024-06-18 13:56:41 +0200

    suma

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get("/hola", (req, res) => {
-  res.send('HOLA MUNDO!')
+function suma(sumando1, sumando2) {
+
+  var sumando1 = 8
+  var sumando2 = 9
+  return resultado
+}
+
+app.get("/hola", (req, res) => { 
+  var sumando1=()
+  var sumando2=()
+
+  suma(sumando1, sumando2)
+  res.send("esta es el resultado:")
 })
+suma (sumando1+sumando2)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
